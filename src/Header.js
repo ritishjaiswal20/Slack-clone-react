57ab977418cebc6ac159e7ae5ae1b226292ec9de
Ritish jaiswal <ritishjaiswal20@gmail.com>
2022-02-13 16:56:04 +0530

    starting build

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+function Header() {
+  return (
+    <div className="header">
+      <div className="header_left">
+        <h1>header</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
