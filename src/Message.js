54a550594192805d4a8523a1c2d4f2f23ee7cad2
Ritish jaiswal <ritishjaiswal20@gmail.com>
2022-02-18 19:50:43 +0530

    styling chat screen

--- src/Message.js
+++ src/Message.js
@@ -5,7 +5,12 @@ function Message({ message, timestamp, user, userImage }) {
     <div className="message">
       <img src={userImage} alt="" />
       <div className="message_info">
-        <h4>{user} timestamp....</h4>
+        <h4>
+          {user}{" "}
+          <span className="message_timeStamp">
+            {new Date(timestamp?.toDate()).toUTCString()}
+          </span>
+        </h4>
         <p>{message}</p>
       </div>
     </div>
