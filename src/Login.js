41d69df43343f8f9b34d89168fa9dffbc26b38a7
Ritish jaiswal <ritishjaiswal20@gmail.com>
2022-02-19 11:32:39 +0530

    google authentication added

--- src/Login.js
+++ src/Login.js
@@ -1,17 +1,28 @@
 import { Button } from "@material-ui/core";
 import React from "react";
 import "./Login.css";
+import { auth, provider } from "./firebase";
 function Login() {
+  const signIn = () => {
+    auth
+      .signInWithPopup(provider)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        alert(error.messsage);
+      });
+  };
   return (
     <div className="login">
       <div className="login_container">
         <img
-          src="https://cdn.mos.cms.futurecdn.net/SDDw7CnuoUGax6x9mTo7dd.jpg"
+          src="https://cdn.dribbble.com/users/116728/screenshots/3373498/slack_animation.gif"
           alt=""
         />
         <h1>Sign in to Ritish room</h1>
         <p>Ritish.slack.com</p>
-        <Button>Sign in with google</Button>
+        <Button onClick={signIn}>Sign in with google</Button>
       </div>
     </div>
   );
